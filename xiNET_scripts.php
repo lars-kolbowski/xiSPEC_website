1da7a59ab18a94e7517b0cf034c666a35b3a012c
Colin Combe <colin.combe@ed.ac.uk>
2015-08-13 13:09:39 +0100

    updated xiNET version

--- xiNET_scripts.php
+++ xiNET_scripts.php
@@ -1,24 +1,2 @@
 		<link rel="stylesheet" href="./css/xiNET.css">
-		<script type="text/javascript" src="./xiNET_2.0.0.js"></script>
-<!--
-        <script type="text/javascript" src="../crosslink-viewer/vendor/d3.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/vendor/colorbrewer.js"></script>
-       	<script type="text/javascript" src="../crosslink-viewer/vendor/FileSaver.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/vendor/rgbcolor.js"></script>   
-        <script type="text/javascript" src="../crosslink-viewer/src/controller/Init.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/src/controller/MouseEvents.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/src/controller/Layout.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/src/controller/Refresh.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/src/controller/ToolTips.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/src/model/Match.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/src/model/Link.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/src/model/Protein.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/src/model/Annotation.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/src/model/ProteinLink.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/src/model/ResidueLink.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/src/controller/ExternalControls.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/src/controller/Rotator.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/src/controller/xiNET_Storage.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/src/controller/ReadCSV.js"></script>
-        <script type="text/javascript" src="../crosslink-viewer/src/controller/Fasta.js"></script>
--->
+		<script type="text/javascript" src="./crosslinkviewer.js"></script>
