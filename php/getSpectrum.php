a0d59a447dcb35bb8909b3947b6b0c7c4aee3989
Lars Kolbowski <lars.b.kolbowski@campus.tu-berlin.de>
2017-06-08 15:27:10 +0200

    minor fix

--- php/getSpectrum.php
+++ php/getSpectrum.php
@@ -5,8 +5,7 @@ session_start();
 $dir = 'sqlite:../dbs/'.session_id().'.db';
 $dbh = new PDO($dir) or die("cannot open the database");
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$query =  "SELECT * FROM jsonReqs WHERE Id=".$_GET['i']." LIMIT 1";
-print $query;
+$query =  "SELECT * FROM jsonReqs WHERE id=".$_GET['i']." LIMIT 1";
 foreach ($dbh->query($query) as $row)
 {
     $postJSON = $row['json'];
