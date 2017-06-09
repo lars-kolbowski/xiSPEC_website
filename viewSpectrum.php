49ab64fdb5735765df1754e8f5d5c99cdc58526c
Lars Kolbowski <lars.b.kolbowski@campus.tu-berlin.de>
2017-06-09 11:37:57 +0200

    sql query fix

--- viewSpectrum.php
+++ viewSpectrum.php
@@ -11,13 +11,8 @@ if (empty($_POST)){
 	$dir = 'sqlite:../dbs/'.session_id().'.db';
 	$dbh = new PDO($dir) or die("cannot open the database");
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$query =  "SELECT * FROM mzids WHERE id=".$_GET['i']." LIMIT 1";
-	foreach ($dbh->query($query) as $row)
-	{
-	    $mzid = $row['mzid'];
-	}
 
-	$query =  "SELECT * FROM jsonReqs WHERE mzid=".$mzid." ORDER BY rank ASC LIMIT 1";
+	$query =  "SELECT * FROM jsonReqs WHERE mzid=0 ORDER BY rank ASC LIMIT 1";
 
 	foreach ($dbh->query($query) as $row)
 	{
