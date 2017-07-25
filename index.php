d4bdd6699249f5cbf1c2e174ea323e108d50153c
Lars Kolbowski <lars.b.kolbowski@campus.tu-berlin.de>
2017-07-25 11:23:46 +0200

    sample spectrum slider

--- index.php
+++ index.php
@@ -4,8 +4,30 @@
 		<?php
 		$pageName = "Home";
 		include("head.php");
+		include("xiSPEC_scripts.php");
 		?>
-		<?php include("xiNET_scripts.php");?>
+	<script type="text/javascript">
+	var slideIndex = 0;
+	function showDiv(n) {
+		oldSlide = slideIndex;
+
+		if(slideIndex+n >= $('.sliderImg').length)
+			slideIndex = 0;
+		else if(slideIndex+n < 0)
+			slideIndex = $('.sliderImg').length-1;
+		else
+			slideIndex += n;
+		$('.sliderImg').eq(oldSlide).fadeOut(200, function(){ $('.sliderImg').eq(slideIndex).fadeIn(200); });
+	}
+
+	$( document ).ready(function() {
+		showDiv(slideIndex);
+		// window.setInterval(function(){
+		// 	showDiv(slideIndex+1);
+		// }, 2000);
+	});
+
+	</script>
 	</head>
 
 	<body>
@@ -14,58 +36,23 @@
 
    	 	<!-- Main -->
    	 	<div id="main">
-	
-   	 	<!-- Intro -->			
+			
    	 				<div class="container">
-   	 					<h1 class="page-header">A tool for visualizing cross-link mass spectrometry spectra.</h1>
-   	 					<div class="external-link">
-							
-							
-							
-<!-- 							<p>xiNET displays:</p>
-							<ul>
-								<li>residue
-							resolution positional information including linkage sites
-							and linked peptides;</li>
-								<li>all types of cross-linking reaction
-							product;</li>
-								<li>ambiguous results;</li>
-								<li>additional sequence information such as domains.</li>
-							</ul> -->
+   	 					<h1 class="page-header">Visualizing (cross-link) mass spectrometry spectra.</h1>
 
-
-						</div><!-- external-link -->
+   	 					<p>A tool for visualizing and analyzing your mass spectrometry data. XiSPEC features automated spectra annotation with intuitive tools for data analysis and hypothesis testing.</p>
+   	 					<p>xiSPEC allows you to upload whole MS datasets and let's you save them for later access (share with colleagues / online access for publication).</p>
+						<div id="spectrumWrapper" style="width:100%;margin-top: 20px;position:relative;">
+ 							<img class="sliderImg" alt="example spectrum" src="example/example_spectrum.svg" style="max-width: 100%;">
+ 							<img class="sliderImg" alt="zoom spectrum" src="example/zoom.png" style="max-width: 100%;display:none;">
+ 							<img class="sliderImg" alt="zoom spectrum" src="example/highlight.png" style="max-width: 100%;display:none;">
+							<button class="sliderBtn slider-left" onclick="showDiv(-1)">&#10094;</button>
+							<button class="sliderBtn slider-right" onclick="showDiv(1)">&#10095;</button>
+ 						</div>
 							
-						<?php //include("citeBar.php");?>
 			 			</div> <!-- CONTAINER -->
-			<div id="networkFrontPage" class="skel-panels-fixed"></div>
 		</div> <!-- MAIN -->
-   	 				<script>//<![CDATA[
-					window.addEventListener('load', function() {
-   	 				
-						var targetDiv = document.getElementById('networkFrontPage');
-						xlv = new xiNET.Controller(targetDiv);
-						
-						d3.text("./data/TFIIF_annot.csv", "text/csv", function(annot) {
-							d3.text("./data/TFIIF_highConf.csv", "text/csv", function(text) {
-								xlv.readCSV(text, null, annot);
-								function makeABarP1(){
-									xlv.proteins.values()[1].setForm(1);
-								}
-								function makeABarP2(){
-									xlv.proteins.values()[2].setForm(1);
-								}
-								setTimeout(makeABarP1, 4000);
-								setTimeout(makeABarP2, 6000);
-							});
-						});
-								
-   	 				
-   	 				}, false);
-   	 				//]]>
-   	 				</script>
-
-   	 				
+  	 				
    	 				
 	</body>
 </html>
