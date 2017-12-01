af0f387d672c07fc0980df52de5b760224790e53
Lars Kolbowski <lars.b.kolbowski@campus.tu-berlin.de>
2017-12-01 15:23:26 +0000

    auth page styling

--- auth.php
+++ auth.php
@@ -5,15 +5,35 @@
 <html>
 	<head>
 		<title>xiSPEC - authenticate</title>
+		<link rel="icon" type="image/ico" href="images/logos/favicon.ico">
+		<link rel="stylesheet" href="./css/auth.css" />
+		<link rel="stylesheet" href="./css/style2.css" />
+		<link rel="stylesheet" type="text/css" href="./css/font-awesome.min.css"/>
+		<link rel="stylesheet" href="./css/validationPage.css">
 	</head>
 	<body>
-			<?php if(isset($_GET['e']) && $_GET['e'] == -1) echo 'wrong password!'; ?>
-		<form action='php/checkAuth.php' method='POST'>
-			The database <?php echo $_SESSION['db']; ?> is private and password protected. Please enter the password below:</br>
-			<label class="flex-row label">
-				Password: <div class="flex-grow"><input class="form-control" required length=30 name="dbPass" type="password" placeholder="Enter password"></div>
-			</label>
-			<input type="submit" class="btn btn-1 btn-1a" value="save">
-		</form>
+
+		<div id="mainView">
+			<div class="mainContent">
+				<div id="spectrumControls">
+					<i class="btn btn-1a btn-topNav fa fa-home fa-xi" style='margin: -2px 2px 2px 2px;' onclick="window.location = 'index.php';" title="Home"></i>
+					<i class="btn btn-1a btn-topNav fa fa-github fa-xi" onclick="window.open('https://github.com/Rappsilber-Laboratory/xiSPEC/issues', '_blank');" title="GitHub issue tracker" style="cursor:pointer;"></i>
+				</div>
+				<div class="container">
+
+					<?php if(isset($_GET['e']) && $_GET['e'] == -1) echo 'wrong password!'; ?>
+					<form action='php/checkAuth.php' method='POST'>
+						The database <?php echo $_SESSION['db']; ?> is private and password protected!</br>
+						<label class="flex-row label">
+							Password: <div class="flex-grow"><input style="max-width: 20em;" class="form-control" required length=30 name="dbPass" type="password" placeholder="Enter password"></div>
+						</label>
+						<div style="text-align:center;">
+							<input type="submit" class="btn btn-2" value="submit">
+						</div>
+					</form>
+
+				</div> <!-- container -->
+			</div> <!-- mainContent -->
+		</div> <!-- mainView -->
 	</body>
 </html>
