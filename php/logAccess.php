fd26a95fe09c3b3f0173fb4a755dca2e133fd7fb
Lars Kolbowski <lars.b.kolbowski@campus.tu-berlin.de>
2017-12-01 14:09:13 +0000

    bugfix: db ids were not logged

--- php/logAccess.php
+++ php/logAccess.php
@@ -1,29 +1,23 @@
 <?php
+	if (session_status() === PHP_SESSION_NONE){session_start();}
+	if(!isset($xiSPECdb))
+		die('No database connection!');
 
-	$dbname = $_GET['db'];
-	$dir = 'sqlite:dbs/xiSPEC.db';
-	$dbh = new PDO($dir) or die("cannot open the database");
-	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-	$stmt = $dbh->prepare("SELECT id FROM databases WHERE name = :name;");
+	$dbname = $_SESSION['db'];
+	$stmt = $xiSPECdb->prepare("SELECT id FROM databases WHERE name = :name;");
 	$stmt->bindParam(':name', $dbname, PDO::PARAM_STR);
-
-
 	$stmt->execute();
+	$dbid = $stmt->fetchColumn();
 
-	if (!$stmt->fetchColumn()) {
+	if (!$dbid) {
 		header("Location: upload.php");
 		exit();
 	}
 	else{
-		$dbid = $stmt->fetchColumn();
 		$ip = $_SERVER['REMOTE_ADDR'];
 		$date = date('Y-m-d H:i:s');
 
-		session_start();
-		$_SESSION['db'] = $dbname;
-
-		$stmt = $dbh->prepare("INSERT INTO access_log ('db_id', 'ip', 'date') VALUES (:dbid, :ip, :dates)");
+		$stmt = $xiSPECdb->prepare("INSERT INTO access_log ('db_id', 'ip', 'date') VALUES (:dbid, :ip, :dates)");
 		$stmt->bindParam(':dbid', $dbid, PDO::PARAM_INT);
 		$stmt->bindParam(':ip', $ip, PDO::PARAM_STR);
 		$stmt->bindParam(':dates', $date, PDO::PARAM_STR);
