7e651a9c1d7f1ad6ddcf25c8d2ae05830b19d83b
Lars Kolbowski <lars.b.kolbowski@campus.tu-berlin.de>
2017-12-01 14:32:48 +0000

    styling

--- legacy.php
+++ legacy.php
@@ -15,15 +15,14 @@
    	 	<!-- Main -->
    	 	<div id="main">
 
-			<div class="container">
+			<div class="container" style="text-align: center;">
 
 				<h1 class="page-header">Legacy xiSPEC</h1>
-				<a href="legacy.spectrumviewer.org">Click here to access the old version of xiSPEC</a>
-				<img class="sliderImg" alt="zoom spectrum" src="images/legacy_xiSPEC.png">
-				<a href="http://sourceforge.net/projects/spectrumviewer/">The source can be found here</a>
+				<a href="http://legacy.spectrumviewer.org">Click here to access the old version of xiSPEC</a></br>
+				<a href="http://legacy.spectrumviewer.org"><img class="sliderImg" alt="zoom spectrum" src="images/legacy_xiSPEC.png"></a></br>
+				<!-- <a href="http://sourceforge.net/projects/spectrumviewer/">The source can be found here</a> -->
 
-
- 			</div> <!-- CONTAINER -->
+			</div> <!-- CONTAINER -->
 		</div> <!-- MAIN -->
 
 
