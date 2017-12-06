0384faf4fd24469fdc8eb00e0af3ca36d9dbb099
Lars Kolbowski <lars.b.kolbowski@campus.tu-berlin.de>
2017-12-06 17:02:56 +0000

    ip fix

--- php/saveDataSet.php
+++ php/saveDataSet.php
@@ -1,7 +1,29 @@
 <?php
+
+function getUserIP(){
+	$client  = @$_SERVER['HTTP_CLIENT_IP'];
+	$forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+	$remote  = $_SERVER['REMOTE_ADDR'];
+
+	if(filter_var($client, FILTER_VALIDATE_IP))
+	{
+		$ip = $client;
+	}
+	elseif(filter_var($forward, FILTER_VALIDATE_IP))
+	{
+		$ip = $forward;
+	}
+	else
+	{
+		$ip = $remote;
+	}
+
+	return $ip;
+}
+
 	if (session_status() === PHP_SESSION_NONE){session_start();}
 	$date = date('Y-m-d H:i:s');
-	$ip = $_SERVER['REMOTE_ADDR'];
+	$ip = getUserIP();
 
 	$dbname = $_POST['dbName'];
 	if($dbname == "") die("no name specified!");
