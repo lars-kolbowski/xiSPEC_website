b6ace32e3341b010fc4723a59713bf6411e35528
Lars Kolbowski <lars.b.kolbowski@campus.tu-berlin.de>
2017-12-11 14:44:22 +0000

    move parser into separate repo

--- php/parseData.php
+++ php/parseData.php
@@ -3,10 +3,11 @@
 	if (session_status() === PHP_SESSION_NONE){session_start();}
 	// unset($_SESSION['db']);
 	$_SESSION['tmpDB'] = session_id();
-	$target_dir = "../../uploads/".session_id()."/";
+	$target_dir = "../uploads/".session_id()."/";
 	$mzid_file = $target_dir . $_POST['mzid_fn'];
 	$mzml_file = $target_dir . $_POST['mzml_fn'];
-	$command = escapeshellcmd('../python_env/bin/python ../py/parser.py '.$mzid_file.' '.$mzml_file. ' '.session_id());
+	$xiSPEC_ms_parser_dir = '../../xiSPEC_ms_parser/';
+	$command = escapeshellcmd($xiSPEC_ms_parser_dir.'python_env/bin/python '.$xiSPEC_ms_parser_dir.'parser.py '.$mzid_file.' '.$mzml_file. ' '.session_id());
 	// die($command);
 	// echo "<br/>";
 	$output = shell_exec($command);
