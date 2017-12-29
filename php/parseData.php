12eb1e9bf6f3e0f8ddf91e23c987bb9fb75624c4
Lars Kolbowski <lars.b.kolbowski@campus.tu-berlin.de>
2017-12-29 18:41:20 +0100

    shell command escaping bugfix

--- php/parseData.php
+++ php/parseData.php
@@ -25,8 +25,8 @@
 	$_SESSION['tmpDB'] = session_id();
 
 	$target_dir = "../uploads/".session_id()."/";
-	$mzid_file = $target_dir . $_POST['mzid_fn'];
-	$mzml_file = $target_dir . $_POST['mzml_fn'];
+	$mzid_file = $target_dir . escapeshellarg($_POST['mzid_fn']);
+	$mzml_file = $target_dir . escapeshellarg($_POST['mzml_fn']);
 
 	$xiSPEC_ms_parser_dir = '../../xiSPEC_ms_parser/';
 
@@ -61,7 +61,7 @@
 	$stmt->execute();
 
 
-	$command = escapeshellcmd($xiSPEC_ms_parser_dir.'python_env/bin/python '.$xiSPEC_ms_parser_dir.'parser.py '.$mzid_file.' '.$mzml_file. ' '.session_id());
+	$command = $xiSPEC_ms_parser_dir.'python_env/bin/python '.$xiSPEC_ms_parser_dir.'parser.py '.$mzid_file.' '.$mzml_file.' '.session_id();
 	// die($command);
 	// echo "<br/>";
 	$output = shell_exec($command);
