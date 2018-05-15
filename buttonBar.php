c02bd2ba403d5e88a88891726ac37d33174f6c5f
Lars Kolbowski <lars.b.kolbowski@campus.tu-berlin.de>
2018-05-15 17:19:40 +0100

    remove unused files

--- buttonBar.php
+++
@@ -1,8 +0,0 @@
-
-					<div class="page-header center">
-						<button class="btn btn-1 btn-1a network-control" onclick="toggleInfoPanel()">Selection Details</button>
-						<button class="btn btn-1 btn-1a network-control" onclick="xlv.autoLayout();">Auto Layout</button>
-						<button class="btn btn-1 btn-1a network-control resetzoom" onclick="xlv.resetZoom();">Reset Zoom</button>
-						<button class="btn btn-1 btn-1a network-control" onclick="xlv.exportSVG('networkSVG');">Export</button>
-						<button class="btn btn-1 btn-1a network-control" id="help" onclick="toggleHelpPanel()">Help</button>
-					</div>
--- citeBar.php
+++
@@ -1,3 +0,0 @@
-					<div class="controlsexamplespage">						
-<p style="text-align:center;">Please <a target="_blank" href="http://www.ncbi.nlm.nih.gov/pubmed/25648531">cite</a> when using xiNET in your work.</p>
-					</div>
