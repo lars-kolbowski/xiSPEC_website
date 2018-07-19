58329f2b8e4a792e18d862cdbe426179d3e0a113
Lars Kolbowski <lars.b.kolbowski@campus.tu-berlin.de>
2018-07-19 11:54:24 +0200

    add requestAnnotation as general feature

--- viewSpectrum.php
+++ viewSpectrum.php
@@ -145,14 +145,14 @@ echo 	'<script type="text/javascript" src="./js/TableWrapperView.js'.$cacheBuste
 				if(tmpDB) db_controls += '<div class="xispec_tooltip_wrapper"><span class="xispec_tooltip_tr" id="saveTooltip">Your dataset is temporary click here if you want to save it for later access!<i class="fa fa-times-circle closeButton"></i></span><i id="saveDB" title="Save" class="xispec_btn xispec_btn-1a xispec_btn-topNav fa fa-floppy-o" aria-hidden="true"></i></div>';
 				else db_controls += '<i id="shareDB" title="Share" class="xispec_btn xispec_btn-1a xispec_btn-topNav fa fa-share-alt" aria-hidden="true"></i>';
 				db_controls += '<i id="toggleSpecList" title="Show/Hide Spectra list" class="xispec_btn xispec_btn-1a xispec_btn-topNav fa fa-bars" aria-hidden="true"></i>';
-				db_controls += '<i id="xispec_revertAnnotation" title="revert to original annotation" class="xispec_btn xispec_btn-topNav fa fa-undo disabled"  aria-hidden="true"></i>';
+				// db_controls += '<i id="xispec_revertAnnotation" title="revert to original annotation" class="xispec_btn xispec_btn-topNav fa fa-undo disabled"  aria-hidden="true"></i>';
 				db_controls += '</span>';
 				$('#xispec_extra_spectrumControls_after').html(db_controls);
 
 				$('#bottomDiv').show();
-				window.initSpinner = new Spinner({scale: 5}).spin (d3.select("#topDiv").node());
+				xiSPEC.initSpinner = new Spinner({scale: 5}).spin (d3.select("#topDiv").node());
 
-				window.TableWrapper = new TableWrapperView({
+				xiSPEC.TableWrapper = new TableWrapperView({
 					model: xiSPEC.SpectrumModel,
 					el:"#bottomDiv",
 					initId: "<?php echo $sid; ?>"		//ToDo: remove? -> not used yet
@@ -163,7 +163,7 @@ echo 	'<script type="text/javascript" src="./js/TableWrapperView.js'.$cacheBuste
 				$('#dbControls').hide();
 				$('#bottomDiv').hide();
 				$('#altDiv').hide();
-				xiSPEC.SpectrumModel.request_annotation(json_req);
+				xiSPEC.SpectrumModel.request_annotation(json_req, true);
 
 			}
 
