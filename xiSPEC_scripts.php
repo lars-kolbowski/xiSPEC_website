e778aaa10ee743ba1297ea2fca96816c38c32c7a
Lars Kolbowski <lars.b.kolbowski@campus.tu-berlin.de>
2018-08-02 16:52:05 +0200

    re add cookie.js

--- xiSPEC_scripts.php
+++ xiSPEC_scripts.php
@@ -1,5 +1,6 @@
 <script type="text/javascript" src="./spectrum/vendor/jquery.min.js"></script>
 <script type="text/javascript" src="./vendor/jquery.easyModal.js"></script>
+<script type="text/javascript" src="./vendor/js.cookie.js"></script>
 <script type="text/javascript" src="./spectrum/vendor/underscore.js"></script>
 <script type="text/javascript" src="./spectrum/vendor/backbone.js"></script>
 <script type="text/javascript" src="./spectrum/vendor/d3.js"></script>
